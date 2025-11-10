5ca8db6d6f7478f0d04333ecc0a63e0884b274a9
Your Name <your.email@example.com>
2025-11-10 13:06:13 +0200

    get all updates from main

--- resources/views/includes/dashboard/main-sidebar.blade.php
+++ resources/views/includes/dashboard/main-sidebar.blade.php
@@ -51,10 +51,10 @@
                         </svg><span class="side-menu__label">الأدوار</span></a>
                 </li>
             {{-- @endcan --}}
-<<<<<<< HEAD
 
-=======
- @can('partner-list') 
+
+
+ @can('partner-list')
                 <li class="slide">
                     <a class="side-menu__item" href="{{ route('partners.index') }}"><svg xmlns="http://www.w3.org/2000/svg"
                             class="side-menu__icon" viewBox="0 0 24 24">
@@ -64,7 +64,7 @@
                                 d="M3 13h8V3H3v10zm2-8h4v6H5V5zm8 16h8V11h-8v10zm2-8h4v6h-4v-6zM13 3v6h8V3h-8zm6 4h-4V5h4v2zM3 21h8v-6H3v6zm2-4h4v2H5v-2z" />
                         </svg><span class="side-menu__label">شركاء النجاح</span></a>
                 </li>
-             @endcan 
+             @endcan
                <li class="slide">
                     <a class="side-menu__item" href="{{ route('certificates.index') }}"><svg xmlns="http://www.w3.org/2000/svg"
                             class="side-menu__icon" viewBox="0 0 24 24">
@@ -93,7 +93,6 @@
                                 d="M3 13h8V3H3v10zm2-8h4v6H5V5zm8 16h8V11h-8v10zm2-8h4v6h-4v-6zM13 3v6h8V3h-8zm6 4h-4V5h4v2zM3 21h8v-6H3v6zm2-4h4v2H5v-2z" />
                         </svg><span class="side-menu__label">مكتبة الفيديوهات</span></a>
                 </li>
->>>>>>> master
         </ul>
     </div>
 </aside>
